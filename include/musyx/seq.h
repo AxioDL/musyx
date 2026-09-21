8855c4463d82fe52d4bb1c78a981079c15d85740
Luke Street <luke@street.dev>
2026-09-21 14:19:23 -0600

    Fix push/pop pragmas

--- include/musyx/seq.h
+++ include/musyx/seq.h
@@ -182,8 +182,7 @@ typedef struct SEQ_PATTERN {
   u32 noteData;   // offset 0xC, size 0x4
 } SEQ_PATTERN;
 
-#pragma push
-#pragma pack(1)
+#pragma pack(push, 1)
 typedef struct CHANNEL_DEFAULTS {
   // total size: 0x9
   u8 pbRange; // offset 0x0, size 0x1
@@ -192,7 +191,7 @@ typedef struct CHANNEL_DEFAULTS {
   u32 lpfUpperFrqBoundary; // offset 0x5, size 0x4
 #endif
 } CHANNEL_DEFAULTS;
-#pragma pop
+#pragma pack(pop)
 
 typedef struct MIDI_CHANNEL_SETUP {
   // total size: 0x5
--- include/musyx/synth.h
+++ include/musyx/synth.h
@@ -116,8 +116,7 @@ typedef struct CTRL_DEST {
   u8 numSource;
 } CTRL_DEST;
 
-#pragma push
-#pragma pack(4)
+#pragma pack(push, 4)
 typedef struct SYNTH_VOICE {
   // total size: 0x404
   SYNTH_QUEUE lowPrecisionJob;            // offset 0x0, size 0xC
@@ -269,8 +268,8 @@ typedef struct synthITDInfo {
   u8 music; // offset 0x0, size 0x1
   u8 sfx;   // offset 0x1, size 0x1
 } synthITDInfo;
+#pragma pack(pop)
 
-#pragma pop
 typedef void (*SYNTH_MESSAGE_CALLBACK)(u32, s32);
 
 extern SND_AUX_CALLBACK synthAuxACallback[8];
